5132f0b7f6dc0b83826c24c8ea8bb16138b97d65
Peter WONG Wing Lun <luiges90@gmail.com>
2013-07-26 20:30:52 +0800

    typed enemy wave generator basis

--- js/typedWave.js
+++ js/typedWave.js
@@ -2,7 +2,7 @@
 
 function typedWave(wave, player, oldEnemy) {
 
-	var createSimpleEnemy = function(){
+	var simple = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(0.15, randomAngle()),
 			color: {h: 0, s: 1, l: 0.5, a: 1}
@@ -11,24 +11,24 @@ function typedWave(wave, player, oldEnemy) {
 		return e;
 	};
 
-	var createChasingEnemy = function(){
+	var chasing = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(0.15, randomAngle()),
-			color: {h: 0.05, s: 1, l: 0.5, a: 1}
+			color: {h: 20, s: 1, l: 0.5, a: 1}
 		}, [alignRotationToMovement, chasePlayer]);
 
 		e.chaseFactor = 0.02;
 		return e;
 	};
 
-	var count = wave;
-
+	var waveData = [];
+	waveData[1] = [simple, simple, simple];
+	waveData[2] = [simple, chasing, simple, chasing];
+	
 	var enemy = [];
-	for (var i = 0; i < count; ++i){
-		var e = createChasingEnemy();
-
-		enemy.push(e);
-	}
+	$.each(waveData[wave], function(){
+		enemy.push(this());
+	});
 
 	return enemy;
 }

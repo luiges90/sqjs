7dae90b7e44da7e8f5405c7c98acbecde9c4c55e
Peter WONG Wing Lun <luiges90@gmail.com>
2013-07-26 21:16:45 +0800

    combine entity, createplayer and createenemy

--- js/entity.js
+++ js/entity.js
@@ -68,3 +68,74 @@ var SqEntity = {
 	},
 
 };
+
+function createPlayer() {
+	var player = Object.create(SqEntity);
+	player.init(TYPE_PLAYER, new b2Vec2(0, 0), 0.12, {
+		linearDamping: 1.5
+	});
+	
+	player.invincibleTimer = 0;
+	player.isInvincible = function(){return this.invincibleTimer > 0;};
+	
+	player.stepAction.push(function(keys, mouse, player, playerBullet, enemy) {
+		this.invincibleTimer--;	
+	});
+	
+	player.revive = function() {
+		this.destroyed = false;
+		this.invincibleTimer = FPS * 3;	
+	};
+	
+	player.fireCooldown = 3;
+	
+	player.stepAction.push(moveByWASD);
+	player.stepAction.push(fireByLeftMouse);
+	
+	player.draw = function() {
+		var canvas = document.getElementById('scene').getContext('2d');
+		var drawX, drawY, drawSize;
+		
+		drawX = this.body.GetPosition().x * 100 + 300;
+		drawY = -this.body.GetPosition().y * 100 + 300;
+		drawSize = this.size * 100;
+		
+		var opacity = this.color.a;
+		if (this.isInvincible() && this.invincibleTimer % 30 < 15) {
+			opacity = 0;
+		}
+	
+		canvas.beginPath();
+		canvas.fillStyle = "hsla(" + this.color.h + ", " + this.color.s * 100 + "%, " + this.color.l * 100 + "%, " + opacity + ")";
+		canvas.arc(drawX, drawY, drawSize, 0, 2 * Math.PI, true);
+		canvas.fill();
+	};
+	
+	return player;
+}
+
+function createEnemy(location, size, options, behaviours) {
+	var e = Object.create(SqEntity);
+	e.init(TYPE_ENEMY, location, size, options);
+
+	$.each(behaviours, function(){
+		e.stepAction.push(this);
+	});
+
+	e.draw = function(){
+		var canvas = document.getElementById('scene').getContext('2d');
+		var drawSize = this.size * 100;
+
+		canvas.save();
+		
+		canvas.fillStyle = "hsla(" + this.color.h + ", " + this.color.s * 100 + "%, " + this.color.l * 100 + "%, " + this.color.a + ")";
+		
+		canvas.translate(this.body.GetPosition().x * 100 + 300, -this.body.GetPosition().y * 100 + 300);
+		canvas.rotate(-this.body.GetAngle());
+		canvas.fillRect(-drawSize, -drawSize, drawSize * 2, drawSize * 2);
+		
+		canvas.restore();
+	};
+	
+	return e;
+}

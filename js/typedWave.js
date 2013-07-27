2ec3eeb1a4b780a16603d024e7ec27da45ba26a3
Peter WONG Wing Lun <luiges90@gmail.com>
2013-07-27 17:50:53 +0800

    change some colors

--- js/typedWave.js
+++ js/typedWave.js
@@ -14,7 +14,7 @@ function typedWave(wave, player, oldEnemy) {
 	var chasing = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(3, randomAngle()),
-			color: {h: 20, s: 1, l: 0.5, a: 1}
+			color: {h: 10, s: 1, l: 0.5, a: 1}
 		}, [alignRotationToMovement, chasePlayer]);
 
 		e.chaseFactor = 0.02;
@@ -24,7 +24,7 @@ function typedWave(wave, player, oldEnemy) {
 	var randomFiring = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(3, randomAngle()),
-			color: {h: 10, s: 1, l: 0.5, a: 1}
+			color: {h: 20, s: 1, l: 0.5, a: 1}
 		}, [alignRotationToMovement, randomFire]);
 
 		e.fireCooldown = 100;
@@ -39,7 +39,7 @@ function typedWave(wave, player, oldEnemy) {
 	var aimedFiring = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(3, randomAngle()),
-			color: {h: 350, s: 1, l: 0.5, a: 1}
+			color: {h: 30, s: 1, l: 0.5, a: 1}
 		}, [alignRotationToMovement, aimedFire]);
 
 		e.fireCooldown = 100;
@@ -63,10 +63,10 @@ function typedWave(wave, player, oldEnemy) {
 		return e;
 	};
 	
-	var hp5AimedFiring = function(){
+	var hp3AimedFiring = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(3, randomAngle()),
-			color: {h: 350, s: 1, l: 0.55, a: 1}
+			color: {h: 30, s: 1, l: 0.55, a: 1}
 		}, [alignRotationToMovement, aimedFire], [hp]);
 
 		e.fireCooldown = 100;
@@ -74,16 +74,16 @@ function typedWave(wave, player, oldEnemy) {
 		e.bulletSize = 0.06;
 		e.bulletSpeed = 5;
 		e.bulletLifetime = 60;
-		e.aimError = deg2rad(10);
+		e.aimError = deg2rad(0);
 		e.bulletBehaviours = [alignRotationToMovement];
-		e.hp = 5;
+		e.hp = 3;
 		return e;
 	};
 	
 	var large = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.4, {
 			linearVelocity: rtToVector(3, randomAngle()),
-			color: {h: 30, s: 1, l: 0.55, a: 1}
+			color: {h: 40, s: 1, l: 0.55, a: 1}
 		}, [alignRotationToMovement], [hp]);
 		
 		e.hp = 5;
@@ -94,7 +94,7 @@ function typedWave(wave, player, oldEnemy) {
 	var small = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.04, {
 			linearVelocity: rtToVector(3, randomAngle()),
-			color: {h: 40, s: 1, l: 0.5, a: 1}
+			color: {h: 50, s: 1, l: 0.5, a: 1}
 		}, [alignRotationToMovement]);
 
 		return e;
@@ -103,7 +103,7 @@ function typedWave(wave, player, oldEnemy) {
 	var fast = function(){
 		var e = createEnemy(randomLocationAvoidRadius(-3 + 0.24, 3 - 0.24, -3 + 0.24, 3 - 0.24, player.body.GetPosition(), 1), 0.12, {
 			linearVelocity: rtToVector(9, randomAngle()),
-			color: {h: 50, s: 1, l: 0.5, a: 1}
+			color: {h: 60, s: 1, l: 0.5, a: 1}
 		}, [alignRotationToMovement]);
 
 		return e;
@@ -118,13 +118,13 @@ function typedWave(wave, player, oldEnemy) {
 	waveData[5] = [chasing, chasing, randomFiring, randomFiring, aimedFiring, aimedFiring];
 	waveData[6] = [hp5, hp5, hp5, chasing, chasing, chasing, chasing];
 	waveData[7] = [hp5, hp5, hp5, aimedFiring, randomFiring, aimedFiring, randomFiring];
-	waveData[8] = [hp5AimedFiring, hp5AimedFiring, hp5AimedFiring, hp5AimedFiring];
-	waveData[9] = [hp5AimedFiring, hp5AimedFiring, hp5AimedFiring, randomFiring, randomFiring, randomFiring, randomFiring];
+	waveData[8] = [hp3AimedFiring, hp3AimedFiring, hp3AimedFiring, hp3AimedFiring];
+	waveData[9] = [hp3AimedFiring, hp3AimedFiring, hp3AimedFiring, randomFiring, randomFiring, randomFiring, randomFiring];
 	waveData[10] = [large, large, large, small, small, small];
 	waveData[11] = [fast, fast, fast, large, large, small, small];
-	waveData[12] = [large, large, large, hp5, hp5, hp5AimedFiring, hp5AimedFiring, hp5AimedFiring];
+	waveData[12] = [large, large, large, hp5, hp5, hp3AimedFiring, hp3AimedFiring, hp3AimedFiring];
 	waveData[13] = [fast, fast, fast, small, small, chasing, chasing, chasing];
-	waveData[14] = [].pushMul(15, simple).pushMul(15, chasing);
+	waveData[14] = [].pushMul(20, chasing);
 	
 	var enemy = [];
 	$.each(waveData[(wave - 1) % waveData.length], function(){

a0c72e10da0b4a5edd47e83ccb56aaa1d786d9d5
Peter WONG Wing Lun <luiges90@gmail.com>
2013-09-01 16:23:31 +0800

    make audio player of better effeciency

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -9,10 +9,17 @@ var AudioPlayer = (function() {
 	return {
 	
 		play: function(file) {
-			$('body').append('<audio src="' + file + '" autoplay>');
-			$('audio').on('ended', function(){
-				$(this).remove();
-			});
+			var className = file.replace(/[^a-zA-Z]/g, '');
+		
+			var existing = $('audio.ended.' + className);
+
+			if (existing.length > 0) {
+				$(existing[0]).removeClass('ended')[0].play();
+			} else {
+				$('<audio>').prop('src', file).prop('autoplay', 'true').prop('class', className).appendTo('body').on('ended', function(){
+					$(this).addClass('ended');
+				});
+			}
 		},
 		
 	};
